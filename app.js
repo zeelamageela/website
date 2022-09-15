8a45dbcfa478efb0c7e6267128be2d5dca31ec2c
Zill Zall <zacklamoureux@gmail.com>
2022-09-15 16:53:41 -0400

    trying out some new stuff

--- app.js
+++ app.js
@@ -1,18 +1,12 @@
-'use strict';
+// add class navbarDark on navbar scroll
+const header = document.querySelector('.navbar');
 
-const switcher = document.querySelector('.btn');
-
-switcher.addEventListener('click', function() {
-    document.body.classList.toggle('light-theme');
-    document.body.classList.toggle('dark-theme');
-
-    const className = document.body.className;
-    if(className == "light-theme"){
-        this.textContent = "Dark";
+window.onscroll = function() {
+    var top = window.scrollY;
+    if(top >=100) {
+        header.classList.add('navbarDark');
     }
-    else{
-        this.textContent = "Light";
+    else {
+        header.classList.remove('navbarDark');
     }
-
-    console.log('current class name: ' + className);
-});
+}
